9ee513af0ea0b2cc838b43b606bb3db31c5f7fe6
Luca Baffa <lb803@mailbox.org>
2019-04-13 11:20:22 +0100

    Very first commit
    Initial commit with working code. The skill is able to retrieve current appointments ("calcurse -a") and read them back. Mycroft responses are kind of dull, some polishing work needed.

---
+++ README.md
@@ -0,0 +1,14 @@
+## Calcurse
+Calcurse wrapper for Mycroft
+
+## Description
+This skill is a [calcurse](https://packages.debian.org/stable/calcurse) wrapper for [Mycroft](https://mycroft.ai/).
+
+## Examples
+ - "What's on my calendar"
+
+
+## Credits
+Luca Baffa
+
+
---
+++ __init__.py
@@ -0,0 +1,68 @@
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#  Luca Baffa - lb803@mailbox.org
+
+from mycroft import MycroftSkill, intent_handler
+from adapt.intent import IntentBuilder
+#~ from mycroft.util import LOG
+
+import re
+from subprocess import run, PIPE
+
+class Calcurse(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    @intent_handler(IntentBuilder('read')
+        .require('what')
+        .require('calendar'))
+    def handle_read(self, message):
+        cmd = run(['calcurse', '-a',
+                   '--format-apt= - %m (from %S to %E)\n',
+                   '--format-recur-apt= * %m (from %S to %E)\n'],
+                  stdout=PIPE)
+
+        data = cmd.stdout.decode('utf-8').split('\n')
+
+        day_apt = []
+        timed_apt = []
+
+        re_day_apt = re.compile('^( \* )(?P<apt>.*)')
+        re_timed_apt = re.compile('^( - )(?P<apt>.[^(]*)\(from (?P<start>[0-9]+:[0-9]+) to (?P<end>[0-9]+:[0-9]+)\)')
+
+
+        for apt in data:
+            match_day_apt = re_day_apt.match(apt)
+            if match_day_apt:
+                day_apt.append(match_day_apt.group('apt'))
+                continue;
+
+            match_timed_apt = re_timed_apt.match(apt)
+            if match_timed_apt:
+                timed_apt.append([match_timed_apt.group('apt'),
+                                  match_timed_apt.group('start')])
+
+        for apt in day_apt:
+            self.speak_dialog('read.day_apt', {'apt': apt})
+
+        for apt in timed_apt:
+            self.speak_dialog('read.timed_apt', {'apt': apt[0],
+                                                 'time': apt[1]})
+
+
+def create_skill():
+    return Calcurse()
+
